6c06e9635c8e0b7e16fd9cf5e658e0cde0ccabe0
daccel22 <33930312+daccel22@users.noreply.github.com>
2020-09-13 17:46:16 -0700

    home

--- js/myscript.js
+++ js/myscript.js
@@ -1,7 +1,17 @@
-function printDate() {
-			var d = new Date(); 
-			document.write("Time ticket: " + d.toLocaleString());
-		}
+/** *************************************************************** * 
+  * JS file for the home page										*
+  *  																*
+  * Author: Yi Chen													*
+  * Last update: 9/11/2020											*
+  * --------------------------------------------------------------- */
+
+
+/** *************************************************************** *
+  * Function_1: sayYourName()										*
+  *																	*
+  * - Ask the visitor's name in advance		 						*
+  *																	*
+  * --------------------------------------------------------------- */
 function sayYourName() {
 	var name = prompt("What is your name?");
 	if(name == null || name.length == 0)
@@ -9,6 +19,27 @@ function sayYourName() {
 	else
 		document.write("<h3>Start your journey here, " + name + "</h3>");
 }
+
+
+/** *************************************************************** *
+  * Function_2: printTime()											*
+  *																	*
+  * - Print the time in the bottom of website 						*
+  *																	*
+  * --------------------------------------------------------------- */
+function printTime() {
+			var d = new Date(); 
+			document.write("Time ticket: " + d.toLocaleString());
+}
+
+
+/** *************************************************************** *
+  * Function_3: confirmAdventure()									*
+  *																	*
+  * - Ask visitor politely whether she/he want to leave home  		*
+  *   for adventure.												*
+  *																	*
+  * --------------------------------------------------------------- */
 function confirmAdventure(form) {
 	var msg = "Are you sure you want to start the adventure?\n";
 	if(confirm(msg)) {
@@ -18,3 +49,25 @@ function confirmAdventure(form) {
 		window.location.href = "index.html";
 	}
 }
+
+
+/** *************************************************************** *
+  * Function_4: confirmUser() - unfinished							*
+  *																	*
+  * - Check whether visitor has the permit to start adventure.  	*
+  *																	*
+  * --------------------------------------------------------------- */
+function dataHandler(info) {
+	var username;
+	for(var i = 0; i < info.username.length; i++) {
+		if(info.username == username) {
+			if(info.password == password)
+				goForAdventure();
+			else
+				printWrongPassword();
+		}
+	}
+	printNUllUsername();
+	
+}
+
